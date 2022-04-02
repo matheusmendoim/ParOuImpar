0d2181cf990d0399b8b655db0658c367c4123cf9
Matheus Fillipe <matheusinho.mfsm@gmail.com>
2022-04-02 15:26:07 -0300

    atualização

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-numero = int(input("Número: "))
-resultado = numero % 2
+while True:
+    try:
+        numero = input("Número: ")
+        resultado = int(numero) % 2
+        if resultado == 0:
+            print(f"O número {numero} é par")
+        else:
+            print(f"O número {numero} é ímpar")
 
-if resultado == 0:
-    print(f"O número {resultado} é par")
-else:
-    print(f"O número {resultado} é ímpar")
+    except ValueError:
+        if numero == 'quit':
+            break
+        else:
+            print("Insira apenas números meu rei")
+
